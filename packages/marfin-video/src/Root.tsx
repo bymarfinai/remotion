62db2a15eded98ae6abffb92407e10fa8d90cc64
bymarfinai <bymarfinai@gmail.com>
2026-09-20 14:43:58 +0700

    feat(marfin-video): expose style categories in Remotion Studio

--- packages/marfin-video/src/Root.tsx
+++ packages/marfin-video/src/Root.tsx
@@ -1,17 +1,33 @@
-import {Composition} from 'remotion';
+import {Composition, Folder} from 'remotion';
 import {MarfinTest} from './MarfinTest';
+import {KECStylePreview} from './styles/kinetic-editorial-collage/StylePreview';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      <Composition
-        id="MarfinTest"
-        component={MarfinTest}
-        durationInFrames={90}
-        fps={30}
-        width={1920}
-        height={1080}
-      />
+      <Folder name="Styles">
+        <Folder name="001-Kinetic-Editorial-Collage">
+          <Composition
+            id="Style-KEC-Preview"
+            component={KECStylePreview}
+            durationInFrames={150}
+            fps={30}
+            width={1080}
+            height={1920}
+          />
+        </Folder>
+      </Folder>
+
+      <Folder name="Tests">
+        <Composition
+          id="MarfinTest"
+          component={MarfinTest}
+          durationInFrames={90}
+          fps={30}
+          width={1920}
+          height={1080}
+        />
+      </Folder>
     </>
   );
 };
