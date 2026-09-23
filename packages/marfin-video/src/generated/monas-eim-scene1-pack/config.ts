15de67887e2f2b1053869208db41af96b478dc95
bymarfinai <bymarfinai@gmail.com>
2026-09-23 14:58:03 +0700

    feat(eim): add Scene 1 layer config

---
+++ packages/marfin-video/src/generated/monas-eim-scene1-pack/config.ts
@@ -0,0 +1,53 @@
+export const EIM = {
+  navy: '#143B72',
+  blue: '#245FF5',
+  paper: '#F6F3EC',
+  outer: '#0A2645',
+  pale: '#E1E7F0',
+};
+
+export const SCENE1 = {
+  headerLeft: {left: 74, top: 58},
+  headerCenter: {left: 540, top: 77, width: 56},
+  headerRight: {right: 72, top: 60},
+
+  question: {
+    left: 96,
+    top: 286,
+    width: 610,
+    fontSize: 86,
+    lineHeight: 0.91,
+  },
+
+  iconic: {
+    left: 88,
+    top: 650,
+    width: 900,
+    fontSize: 196,
+    lineHeight: 0.78,
+  },
+
+  supporting: {
+    left: 116,
+    top: 1210,
+    width: 245,
+    fontSize: 27,
+    lineHeight: 1.0,
+  },
+
+  cityCircle: {
+    right: -250,
+    top: 940,
+    size: 900,
+  },
+
+  cityImage: {
+    left: 40,
+    top: 170,
+    width: 820,
+    height: 620,
+  },
+
+  footerDash: {left: 92, bottom: 84, width: 58},
+  footerText: {left: 92, bottom: 48},
+};
